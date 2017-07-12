2bf448799292fb6764a55d5d1c0361ecf6cb6ef3
Innocent Asiimwe <asiimwe@outlook.com>
2017-07-12 13:03:51 +0300

    Add static files and base template

--- app/classes/user.py
+++ app/classes/user.py
@@ -1,3 +1,4 @@
+from .bucketlist import BucketList
 class User:
     """Creates the user object and has methods for creating and viewing bucketlists and activities"""
 
@@ -6,7 +7,8 @@ class User:
 
     def create_bucketlist(self, name, description):
         """method creates a new bucketlist for current user with the given name and description"""
-        pass
+        self.bucketlists.append(BucketList(name,description))
+        
 
     def create_activity(self, bucketlist_name, activity_name, activity_status):
         """method creates a new activity with activity_name and activity_status in bucketlist of provided name"""
