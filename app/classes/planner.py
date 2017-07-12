02189ebee5f44ef8de123afb3ed7a323997db21c
Innocent Asiimwe <asiimwe@outlook.com>
2017-07-12 22:43:30 +0300

    Add tests for the login_user and log_out user methods of the Planner class

--- app/classes/planner.py
+++ app/classes/planner.py
@@ -30,7 +30,7 @@ class Planner:
 
     def logout_user(self, username):
         """A method to logout a user with given username"""
-        pass
+        
     
     def user_exists(self, username):
         """method checks if the user already exists basing on username"""
--- tests/test_planner.py
+++ tests/test_planner.py
@@ -1,11 +1,13 @@
 import unittest
 from app.classes.planner import Planner
+from app.classes.user import User
 
 class TestPlanner(unittest.TestCase):
     """Class to handle tests for the planner class"""
 
     def SetUp(self):
         self.plan = Planner()
+        self.user = User('Jane', 'Jackson', 'jane', '123', 'j@j.com')
     
     def test_create_user_success(self):
         """Test a user is created successfully"""
@@ -17,6 +19,14 @@ class TestPlanner(unittest.TestCase):
         user1 = self.plan.create_user('Innocent', 'Asiimwe', 'inno', '1234', 'asiimwe@outlook.com')
         user2 = self.plan.create_user('Innocent', 'Leone', 'inno', '1234', 'leone@outlook.com')
         self.assertEqual([user1, user2], ['Success','Fail'])
+
+    def test_login_user_success(self):
+        """Method updates the loged_in variable of the Planner class"""
+        online_before = len(self.plan.loged_in)
+        self.plan.login_user('jane', '123')
+        online_after = len(self.plan.loged_in)
+        self.assertEqual(online_after, online_before + 1)
+        
 
     
 
