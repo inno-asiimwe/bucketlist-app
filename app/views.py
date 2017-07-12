99b675732775a91ebc6f5dca3ed1e6904fcc9e34
Innocent Asiimwe <asiimwe@outlook.com>
2017-07-12 23:35:47 +0300

    Implement view for logout

--- app/views.py
+++ app/views.py
@@ -8,10 +8,10 @@ def index():
     if 'name' in session:
         return render_template('home.html')
     else:
-        return render_template('login.html')
+        return redirect(url_for('log_in'))
 
-@app.route('/', methods=['GET', 'POST'])
-def login_in():
+@app.route('/login', methods=['GET', 'POST'])
+def log_in():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -23,7 +23,12 @@ def login_in():
             flash("Login failed ...")
             return render_template('login.html')
     else:
-        return "Get"
+        return render_template('login.html')
+@app.route('/logout', methods=['GET', 'POST'])
+def log_out():
+    session.pop('username', None)
+    return redirect(url_for('log_in'))
+
 @app.route('/createuser', methods = [ 'GET','POST'])
 def create_user():
     if request.method == 'POST':
